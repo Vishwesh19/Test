60fd461ea48f15d5c4449843908bdcc64520aaa5
Vishwesh19 <45453807+Vishwesh19@users.noreply.github.com>
2021-06-16 22:51:43 +0530

    Create ValueError.py

---
+++ ValueError.py
@@ -0,0 +1,12 @@
+def prod(a,b,bound):
+	m = a*b
+	
+	if(m<=bound):
+			return m
+	else:
+		raise ValueError(f'multiplication of {a} and {b} not possible with bound {bound}') 
+
+    
+if __name__ == 'main':
+  prod(4,3,9)
+  prod(2,3,7)
